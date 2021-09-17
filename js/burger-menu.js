1f621fdd4a6b76071b0817c1ea59763a362b6b90
alvina-dr <alvinadr24@gmail.com>
2021-09-17 21:14:41 +0200

    ajout du menu burger fonctionnel en beta, bugs à tester dans les prochaines versions

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,15 +1,19 @@
-var btnPopup = document.getElementById('btnOpenMenu');
 var btnClose = document.getElementById('btnCloseMenu');
+var btnPopup = document.getElementById('btnOpenMenu');
 var overlay = document.getElementById('popup');
 
-btnOpenMenu.addEventListener('click', openMenu);
 btnCloseMenu.addEventListener('click', closeMenu);
+btnOpenMenu.addEventListener('click', openMenu);
 
-
-function openMenu() {
-    overlay.style.display = 'block';
-}
 
 function closeMenu() {
     overlay.style.display = 'none';
+    overlay.style.opacity = 0;
+    overlay.style.visibility = "hidden";
+}
+
+function openMenu() {
+    overlay.style.display = 'flex';
+    overlay.style.opacity = 1;
+    overlay.style.visibility = "visible";
 }
